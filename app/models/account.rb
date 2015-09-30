d106097661b55c8e7f45c3353bd8e2ef3b2c2615
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-09-30 10:34:15 -0300

    adicionado usuário ao criar uma conta e relacionando usuário com conta

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,12 +1,19 @@
 class Account < ActiveRecord::Base
 	RESTRICTED_SUBDOMAINS = %w(wwww)
 
+	belongs_to :owner, class_name: 'User'
+
+	validates :owner, presence: true
+
 	validates :subdomain, presence: true, 
 		uniqueness: { case_sensitive: false },
 		format: { with: /\A[\w\-]+\Z/i, message: 'subdominio com caracteres invalidos' },
 		exclusion: { in: RESTRICTED_SUBDOMAINS, message: 'subdominio restrito' }
 
 	before_validation :downcase_subdomain
+
+
+	accepts_nested_attributes_for :owner
 
 private 
 	def downcase_subdomain
