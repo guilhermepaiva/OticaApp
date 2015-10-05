d57429ed47aca79756fd1e6e5861be0ad0479a2e
Gilberto Trindade <g.trindade@gmail.com>
2015-10-05 01:31:01 -0300

    associações consertadas

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
 	#self.inheritance_column = nil #nao tenho certeza disso aqui, vi no stackoverflow
 	#belongs_to :account
+  has_many :orders
+  has_many :attendance, through: :orders
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :attendances
+  has_many :clients, through: :attendances
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, presence: true
+  
   
 end
