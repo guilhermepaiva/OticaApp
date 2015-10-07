ffd5bacb574c6c4eb387c03a1561fcb65149541a
Gilberto Trindade <g.trindade@gmail.com>
2015-10-07 18:38:22 -0300

    model Attendance apagado

--- app/models/attendance.rb
+++
@@ -1,7 +0,0 @@
-class Attendance < ActiveRecord::Base
-  #belongs_to :account
-  belongs_to :user
-  belongs_to :client
-  has_many :orders
-  has_many :products, through: :orders
-end
--- db/migrate/20151004142718_create_orders.rb
+++ db/migrate/20151004142718_create_orders.rb
@@ -1,9 +1,12 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
+      t.integer :user_id
       t.integer :product_id
-      t.integer :attendance_id
+      t.integer :client_id
       t.integer :quantity
+      
+      t.index [:user_id, :product_id, :client_id]
 
       t.timestamps null: false
     end
