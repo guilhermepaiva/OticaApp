14a8ebb8d76757b2868facca15d11dfc7b64fa4a
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-10-23 19:51:33 -0300

    parametros nao em application.rb e sim em apartment.rb

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -1,3 +1,9 @@
+require 'apartment/elevators/subdomain'
+
 Apartment.configure do |config|
 	config.excluded_models = ['Account']
+
+	Rails.application.config.middleware.use 'Apartment::Elevators::Subdomain'
+
+	Apartment::Elevators::Subdomain.excluded_subdomains = ['www']
 end
