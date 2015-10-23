1ffc83f74d05a327dc4a34e059ed4c30a133597a
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-10-23 19:11:35 -0300

    added www.optk como excluded

--- config/initializers/apartment/subdomain_exclusions.rb
+++ config/initializers/apartment/subdomain_exclusions.rb
@@ -1,2 +1,2 @@
 # config/initializers/apartment/subdomain_exclusions.rb
-Apartment::Elevators::Subdomain.excluded_subdomains = ['www', 'optk']
+Apartment::Elevators::Subdomain.excluded_subdomains = ['www', 'optk', 'www.optk']
