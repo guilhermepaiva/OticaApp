2eab48189fb5323c70d495c818a53be3568ea280
Gilberto Trindade <g.trindade@gmail.com>
2015-10-26 08:55:26 -0300

    Update apartment.rb

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -3,7 +3,7 @@ require 'apartment/elevators/subdomain'
 Apartment.configure do |config|
 	config.excluded_models = ['Account']
 
-	Rails.application.config.middleware.use 'Apartment::Elevators::Subdomain'
+	# Rails.application.config.middleware.use 'Apartment::Elevators::Subdomain'
 
-	Apartment::Elevators::Subdomain.excluded_subdomains = ['www']
+	# Apartment::Elevators::Subdomain.excluded_subdomains = ['www']
 end
