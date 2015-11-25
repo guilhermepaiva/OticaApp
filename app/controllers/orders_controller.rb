d3f0171373f1d16afd4599dc383a337d36e1482a
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-11-25 09:27:21 -0300

    added order.save

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -23,6 +23,7 @@ class OrdersController < ApplicationController
       stock_decremented = product.stock_quantity - quantity
       product.update_attribute(:stock_quantity, stock_decremented)
       if !Order.create!(user_id: current_user.id, client_id: params[:order][:client], product_id: id, quantity: quantity)
+        @order.save
         respond_to do |format|
           format.html { render :new}
         end
